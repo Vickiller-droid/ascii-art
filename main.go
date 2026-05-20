e37749acc7dcb92a246669f425fac1b2c03972b4
valabi <victordanielgamco.com>
2026-05-20 12:02:59 +0100

    Third commit

--- main.go
+++ main.go
@@ -7,10 +7,29 @@ import (
 )
 
 func main() {
-	content,  err := os.ReadFile("standard.txt")
+	content, err := os.ReadFile("standard.txt")
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		return
 	}
-	fmt.Println(string(content))
+	file := strings.Split(string(content), "\n")
+
+	charMap := map[rune][]string{}
+
+	for i := 32; i < 126; i++ {
+		start := (i-32)*9 + 1
+		end := start + 8
+		charMap[rune(i)] = file[start:end]
+	}
+	input := "hello"
+	for z := 0; z < 8; z++ {
+		for _, f := range input {
+
+			fmt.Print(charMap[f][z])
+		}
+		fmt.Println()
+	}
+	for _, v := range charMap['h'] {
+		fmt.Println(v)
+	}
 }
