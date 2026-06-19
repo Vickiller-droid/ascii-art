2a618813a06395bcd5cdbec274884d2aba6da89f
valabi <victordanielgamco@gmail.com>
2026-06-19 09:19:16 +0100

    final commit

--- banner.go
+++ banner.go
@@ -7,10 +7,22 @@ import (
 )
 
 func loadBanner(banner string) []string {
+	banner = strings.TrimSuffix(banner, ".txt")
 	content, err := os.ReadFile(banner + ".txt")
 	if err != nil {
-		fmt.Println("Error reading file:", err)
+		fmt.Println("Error reading file:")
 		os.Exit(1)
 	}
-	return strings.Split(string(content), "\n")
+	lines := strings.Split(string(content), "\n")
+	if len(content) == 0 {
+		fmt.Println("Error: Empty file")
+		os.Exit(1)
+	}
+
+	if len(lines) != 855 {
+		fmt.Println("Error: Incomplete or corrupted file")
+		os.Exit(1)
+	}
+	return lines
 }
+
--- validate.go
+++ validate.go
@@ -5,18 +5,23 @@ import (
 	"os"
 )
 
-func validate() (string, string){
+func validate() (string, string) {
 	if len(os.Args) < 2 || len(os.Args) > 3 {
-		fmt.Println("Usage: go run . \"Hello\"" )
+		fmt.Println("Usage: go run . \"Hello\"")
 		os.Exit(1)
 	}
 
 	input := os.Args[1]
 	banner := "standard"
 
+	if banner != "standard" && banner != "shadow" && banner != "thinkertoy" {
+		fmt.Println("Error: invalid banner. Use standard, shadow or thinkertoy")
+		os.Exit(1)
+	}
+
 	if len(os.Args) == 3 {
 		banner = os.Args[2]
 	}
 
-	return  input, banner
+	return input, banner
 }
