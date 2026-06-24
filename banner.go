b2b3e96735c1a80cdc9997e2ce80b86e828d4dfb
valabi <victordanielgamco.com>
2026-06-24 15:41:09 +0100

    Final Commit

--- banner.go
+++ banner.go
@@ -7,22 +7,23 @@ import (
 )
 
 func loadBanner(banner string) []string {
-	banner = strings.TrimSuffix(banner, ".txt")
 	content, err := os.ReadFile(banner + ".txt")
+
 	if err != nil {
-		fmt.Println("Error reading file:")
+		fmt.Println("Error reading file")
 		os.Exit(1)
 	}
+
 	lines := strings.Split(string(content), "\n")
+
 	if len(content) == 0 {
-		fmt.Println("Error: Empty file")
+		fmt.Println("Error: Empty File")
 		os.Exit(1)
 	}
 
 	if len(lines) != 855 {
-		fmt.Println("Error: Incomplete or corrupted file")
+		fmt.Println("Error: Incomplete or corrupt file")
 		os.Exit(1)
 	}
 	return lines
 }
-
--- validate.go
+++ validate.go
@@ -1,8 +1,9 @@
-package main
+package main 
 
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func validate() (string, string) {
@@ -12,16 +13,19 @@ func validate() (string, string) {
 	}
 
 	input := os.Args[1]
-	banner := "standard"
-
-	if banner != "standard" && banner != "shadow" && banner != "thinkertoy" {
-		fmt.Println("Error: invalid banner. Use standard, shadow or thinkertoy")
-		os.Exit(1)
-	}
+	banner := "standard" 
 
 	if len(os.Args) == 3 {
 		banner = os.Args[2]
 	}
 
+	banner = strings.TrimSuffix(banner, ".txt")
+
+	if banner != "standard" &&
+	banner != "shadow" &&
+	banner != "thinkertoy" {
+		fmt.Println("Invalid font: Use standard, shadow or thinkertoy with \".txt\", e.g standard.txt or standard alone")
+		os.Exit(1)
+	}
 	return input, banner
 }
