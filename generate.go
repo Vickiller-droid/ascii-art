2d30d3f0ca59fc95393b0db388cc87e949e8fe84
valabi <victordanielgamco@gmail.com>
2026-06-25 17:58:02 +0100

    refactor: solved newline edge case

--- generate.go
+++ generate.go
@@ -1,19 +1,28 @@
-package main 
+package main
 
-import(
-	"fmt"
+import (
 	"strings"
+	"fmt"
 )
-
 func generate(input string, lines []string) {
+	if input == "" {
+		return
+	}
+
+	if strings.Trim(input, "\\n") == "" {
+		fmt.Print(strings.Repeat("\n", strings.Count(input, "\\n")))
+		return
+	}
+
 	words := strings.Split(input, "\\n")
 
 	for _, word := range words {
-		if strings.TrimSpace(word) == "" {
+		if word == "" {
 			fmt.Println()
 			continue
 		}
-		for i := 0; i < 8;i++ {
+
+		for i := 0; i < 8; i++ {
 			for _, char := range word {
 				fmt.Print(lines[(int(char)-32)*9+1+i])
 			}
